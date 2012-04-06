37ec1c6d16937abe4042bd5a64246e62877b609f
Brad Griffith <braddgriffith@gmail.com>
2012-04-06 09:37:58 -0700

    Initial Commit

---
+++ Powerball Results/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Powerball Results
+//
+//  Created by Brad Grifffith on 4/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
